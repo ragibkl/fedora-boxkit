4dda6266d6c5accf480f6a870215d42caa0df0bd
Ragib Badaruddin <ragib.badaruddin@gmail.com>
2024-03-18 16:38:40 +0800

    update code sh

--- code.sh
+++ code.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 ID=$(echo -n ${CONTAINER_ID:-default} | od -An -t x1 | tr -d ' ' | cat)
-DIR=$(pwd)/$1
 
-distrobox-host-exec code --folder-uri vscode-remote://attached-container+${ID}/$DIR
+if [ -z "$1" ]; then
+    distrobox-host-exec code --folder-uri vscode-remote://attached-container+${ID}
+else
+    DIR=$(pwd)/$1
+    distrobox-host-exec code --folder-uri vscode-remote://attached-container+${ID}/$DIR
+fi
